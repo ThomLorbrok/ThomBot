c459f994820ab894fc79a5d58c3dbde9b51bb281
ThomLorbrok <38364156+ThomLorbrok@users.noreply.github.com>
2018-04-14 08:58:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -30,3 +30,5 @@ bot.on('message', message => {
           if (message.content === 'thombot la table de 1 jusque 10') {
         message.channel.send('Goooo! \n 1x1=1\n1x2=2\n1x3=3\n1x4=4\n1x5=5\n1x6=6\n1x7=7\n1x8=8\n1x9=9\n1x10=10')
     }
+
+  )}
