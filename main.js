d93a3ae76e21c091559b9466bb36125b9675ec4b
ThomLorbrok <38364156+ThomLorbrok@users.noreply.github.com>
2018-04-14 17:30:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -44,4 +44,14 @@ if(message.content === prefix + "infodiscord")
        .setColor("0x0000FF")
     message.channel.sendEmbed(embed)
 
-         })
+     }
+
+ else if(spliteMessage[0] === "!ban") {
+     if(spliteMessage.lenght === 2) {
+         message.guild.ban(message.mention.users.first());
+    }
+     else
+         message.reply("Problème dans les paramètres");
+      }
+   }
+});
